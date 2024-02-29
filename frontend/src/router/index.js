9bd14299006c6db0464e5242fe93555cf7a14c68
Shane Pringle <shanepringle1000@gmail.com>
2024-02-29 00:18:33 -0500

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,7 @@ const routes = [
   { path: '/control', name: 'Control', component: () => import('@/views/Control.vue'), meta:{ transition: 'fade'} },
   { path: '/live', name: 'Live', component: () => import('@/views/Live.vue'), meta:{ transition: 'fade'} },
   { path: '/analysis', name: 'Analysis', component: () => import('@/views/Analysis.vue'), meta:{ transition: 'fade'} },
+  
   // ADD NEW ROUTES HERE
   
 ]
