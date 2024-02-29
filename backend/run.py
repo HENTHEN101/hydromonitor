f33222eccb938ff0c2486d2a55bd76401691c592
Shane Pringle <shanepringle1000@gmail.com>
2024-02-29 00:06:46 -0500

    new update

--- backend/run.py
+++ backend/run.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     # START MQTT CLIENT 
     Mqtt.client.loop_start()
 
+
     # RUN FLASK APP
     app.run(debug=Config.FLASK_DEBUG, host=Config.FLASK_RUN_HOST, port=Config.FLASK_RUN_PORT)
     
